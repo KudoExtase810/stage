dee303557933b9c7084380a67fd6a71b0a864e0b
KudoExtase810 <alaeddino4892@gmail.com>
2023-08-01 19:34:17 +0100

    updates

--- client/src/components/auth/LoginForm.tsx
+++ client/src/components/auth/LoginForm.tsx
@@ -1,4 +1,28 @@
+import axios from "axios";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+
 const LoginForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const creds = { username, password };
+        // try {
+        //     const res = await axios.post(
+        //         "http://localhost:6009/users/create",
+        //         creds
+        //     );
+        //     toast.success("Logged in successfully");
+        // } catch (error: any) {
+        //     error.response.data.message &&
+        //         toast.error(error.response.data.message);
+        // }
+        try {
+        } catch (error) {}
+    };
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -10,15 +34,20 @@ const LoginForm = () => {
                         eaque aut repudiandae et a id nisi.
                     </p>
                 </div>
-                <form className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <form
+                    className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100"
+                    onSubmit={handleLogin}
+                >
                     <div className="card-body">
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text">Username</span>
                             </label>
                             <input
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
                                 type="text"
-                                placeholder="email"
+                                placeholder="username"
                                 className="input input-bordered"
                             />
                         </div>
@@ -27,21 +56,17 @@ const LoginForm = () => {
                                 <span className="label-text">Password</span>
                             </label>
                             <input
-                                type="text"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                type="password"
                                 placeholder="password"
                                 className="input input-bordered"
                             />
-                            <label className="label">
-                                <a
-                                    href="#"
-                                    className="label-text-alt link link-hover"
-                                >
-                                    Forgot password?
-                                </a>
-                            </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary" type="submit">
+                                Login
+                            </button>
                         </div>
                     </div>
                 </form>
